168e714215d6bca9d791a374e32f25626cbad28a
Alfex4936 <ikr@kakao.com>
2024-07-11 19:28:29 +0900

    Match lyrics

---
+++ reactjs/build/precache-manifest.3dc1335983f15fcf26ae75043432203f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531f6ed42f9d8debbb55224bbb7329b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5dbd02088a148ee20a7",
+    "url": "/static/css/2.d7faf78c.chunk.css"
+  },
+  {
+    "revision": "99d71f4cd8755e7aa307",
+    "url": "/static/css/main.a9ad73e4.chunk.css"
+  },
+  {
+    "revision": "c5dbd02088a148ee20a7",
+    "url": "/static/js/2.9ddb61d5.chunk.js"
+  },
+  {
+    "revision": "99d71f4cd8755e7aa307",
+    "url": "/static/js/main.4a5d1e1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -10,7 +10,7 @@ import {
   RefinementList,
   SearchBox,
   Snippet,
-  SortBy
+  SortBy,
 } from "react-instantsearch";
 import "./App.css";
 import "./Modal.css";
@@ -27,49 +27,155 @@ const refreshPage = () => {
   window.location.reload();
 };
 
-const App = () => (
-  <div className="ais-InstantSearch">
-    <h1>
-      <a
-        href="#"
-        onClick={refreshPage}
-        style={{ cursor: "pointer", color: "inherit", textDecoration: "inherit" }}
-      >
-        TJ미디어 노래 검색 Demo
-      </a>
-    </h1>
-    <h2>
-      노래 검색{" "}
-      <span role="img" aria-label="emoji">
-        🎤
-      </span>
-    </h2>
-    <p>TJ미디어 노래들을 검색해보세요. (정확하지 않을 수 있음, 2024년 7월까지)</p>
-    <InstantSearch indexName="songs" searchClient={searchClient}>
-      <div className="left-panel">
-        {/* <ClearRefinements /> */}
-        <SortBy
-          defaultrefinement="songs"
-          items={[{ value: "songs", label: "관련순" }]}
-        />
-        <h2>국가</h2>
-        <RefinementList attribute="country" />
-        <Configure
-          hitsPerPage={8}
-          attributesToSnippet={["description:50"]}
-          snippetEllipsisText={"..."}
-        />
-      </div>
-      <div className="right-panel">
-        <SearchBox />
-        <Hits hitComponent={Hit} />
-        <Pagination showLast={true} />
-      </div>
-    </InstantSearch>
-  </div>
-);
+const validInitialConsonants = new Set([
+  "ㄱ",
+  "ㄲ",
+  "ㄴ",
+  "ㄷ",
+  "ㄸ",
+  "ㄹ",
+  "ㅁ",
+  "ㅂ",
+  "ㅃ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅉ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+]);
+
+const doubleConsonants = {
+  ㄳ: ["ㄱ", "ㅅ"],
+  ㄵ: ["ㄴ", "ㅈ"],
+  ㄶ: ["ㄴ", "ㅎ"],
+  ㄺ: ["ㄹ", "ㄱ"],
+  ㄻ: ["ㄹ", "ㅁ"],
+  ㄼ: ["ㄹ", "ㅂ"],
+  ㄽ: ["ㄹ", "ㅅ"],
+  ㄾ: ["ㄹ", "ㅌ"],
+  ㄿ: ["ㄹ", "ㅍ"],
+  ㅀ: ["ㄹ", "ㅎ"],
+  ㅄ: ["ㅂ", "ㅅ"],
+};
+
+const segmentConsonants = input => {
+  const result = [];
+
+  for (const r of input) {
+    if (validInitialConsonants.has(r)) {
+      result.push(r);
+    } else if (doubleConsonants[r]) {
+      result.push(...doubleConsonants[r]);
+    } else {
+      result.push(r);
+    }
+  }
+
+  return result.join("");
+};
+
+
+const App = () => {
+  // const [filterLyrics, setFilterLyrics] = useState(false);
+
+  const queryHook = (query, refine) => {
+    const segmentedQuery = segmentConsonants(query);
+    // const finalQuery = filterLyrics ? `lyrics:${segmentedQuery}` : segmentedQuery;
+    refine(segmentedQuery);
+  };
+  
+  return (
+    <div className="ais-InstantSearch">
+      <h1>
+        <a
+          href="#"
+          onClick={refreshPage}
+          style={{
+            cursor: "pointer",
+            color: "inherit",
+            textDecoration: "inherit",
+          }}
+        >
+          TJ미디어 노래 검색 Demo
+        </a>
+      </h1>
+      <h2>
+        노래 검색{" "}
+        <span role="img" aria-label="emoji">
+          🎤
+        </span>
+      </h2>
+      <p>
+        TJ미디어 노래들을 검색해보세요. (정확하지 않을 수 있음, 2024년 7월까지)
+      </p>
+      <InstantSearch indexName="songs" searchClient={searchClient}>
+        <div className="left-panel">
+          {/* <ClearRefinements /> */}
+          <SortBy
+            defaultrefinement="songs"
+            items={[{ value: "songs", label: "관련순" }]}
+          />
+          <h2>국가</h2>
+          <RefinementList attribute="country" />
+          <Configure
+            hitsPerPage={8}
+            attributesToSnippet={["description:50"]}
+            snippetEllipsisText={"..."}
+            // filters={filterLyrics ? 'lyrics != ""' : ''}
+          />
+          {/* <button onClick={() => setFilterLyrics(!filterLyrics)}>
+            {filterLyrics ? "Show All Results" : "Show Only Lyrics Matches"}
+          </button> */}
+        </div>
+        <div className="right-panel">
+          <SearchBox
+            autoFocus
+            placeholder={"노래를 검색해보세요..."}
+            queryHook={queryHook}
+          />
+          <Hits hitComponent={Hit} />
+          <Pagination showLast={true} totalPages={14} />
+        </div>
+      </InstantSearch>
+    </div>
+  );
+};
 
 const Hit = ({ hit }) => {
+  // Function to extract and display matched lyrics snippet
+  const getMatchedLyricsSnippet = (highlightedLyrics) => {
+    const snippet = highlightedLyrics.value;
+
+    // Find the position of the highlighted tag
+    const highlightTagStart = snippet.indexOf('<mark>');
+    const highlightTagEnd = snippet.indexOf('</mark>', highlightTagStart) + 7; // length of </mark> is 7
+
+    if (highlightTagStart === -1 || highlightTagEnd === -1) {
+      return null;
+    }
+
+    // Extract the context around the highlighted part
+    const contextBefore = snippet.slice(Math.max(0, highlightTagStart - 30), highlightTagStart);
+    const highlightedText = snippet.slice(highlightTagStart, highlightTagEnd);
+    const contextAfter = snippet.slice(highlightTagEnd, Math.min(snippet.length, highlightTagEnd + 30));
+
+    // Combine the context and the highlighted part
+    const contextSnippet = `가사: ... ${contextBefore} ${highlightedText} ${contextAfter} ...`;
+
+    return (
+      <div className="hit-lyrics-snippet">
+        <span dangerouslySetInnerHTML={{ __html: contextSnippet }} />
+      </div>
+    );
+  };
+
+  const lyricsSnippet = hit._highlightResult && hit._highlightResult.lyrics ? getMatchedLyricsSnippet(hit._highlightResult.lyrics) : null;
+
   return (
     <div key={hit.id}>
       <div className="hit-title">
@@ -79,6 +185,7 @@ const Hit = ({ hit }) => {
       <div className="hit-singer">
         <Snippet attribute="singer" hit={hit} tagName="mark" />
       </div>
+      {lyricsSnippet}
       {hit.youtube_url && (
         <div className="hit-info">
           <a
